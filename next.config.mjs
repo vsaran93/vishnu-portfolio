70ee3d02d40f87be03cf9f3d627cd51dbe8fbca3
vsaran93 <vishnusaran37@gmail.com>
2025-09-27 23:25:32 +0100

    config file change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // this is required for `next export` to work
+    basePath: '/vishnu-portfolio',
+    trailingSlash: true,
 };
 
 export default nextConfig;
