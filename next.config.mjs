724c7fbcfc39b053a3db69585a8c1053e08e2dff
vsaran93 <vishnusaran37@gmail.com>
2025-09-28 10:58:35 +0100

    fix image path issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // this is required for `next export` to work
+    assetPrefix: '/vishnu-portfolio',
     basePath: '/vishnu-portfolio',
     trailingSlash: true,
 };
