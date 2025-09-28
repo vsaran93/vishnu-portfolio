29334ee059f523cc849a40128dee57509e8753a1
vsaran93 <vishnusaran37@gmail.com>
2025-09-28 20:43:03 +0100

    added back the basic path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // this is required for `next export` to work
+    basePath: '/vishnu-portfolio',
     trailingSlash: true,
 };
 
