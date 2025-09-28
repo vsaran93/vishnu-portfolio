9ba4733e43bb09265b8e1252a368607544f3de2b
vsaran93 <vishnusaran37@gmail.com>
2025-09-28 11:03:20 +0100

    fix the asset path

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export', // this is required for `next export` to work
     assetPrefix: '/vishnu-portfolio',
-    basePath: '/vishnu-portfolio',
+    basePath: '/vishnu-portfolio/',
     trailingSlash: true,
 };
 
